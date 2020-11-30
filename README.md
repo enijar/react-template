b37f774b3ba69a1d4aad6b957fb176fe0a324cff
Enijar <TheEnijar@gmail.com>
2020-11-30 22:44:00 +0000

    feat: better build process

--- README.md
+++ README.md
@@ -13,3 +13,32 @@ cp .env.example .env
 npm install
 npm start # http://localhost:3000
 ```
+
+### Deploy
+
+NGINX
+
+```nginx
+server {
+    listen 80;
+    server_name _;
+
+    location / {
+        index index.html;
+        root /var/www/app/releases/latest;
+        try_files $uri $uri/ /index.html =404;
+        expires 1M;
+        access_log off;
+        add_header Cache-Control "public";
+        absolute_redirect off;
+    }
+}
+```
+
+Build
+
+```bash
+cp .env.example .env
+npm install
+npm run build # outputs to releases/latest
+```
--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,37 @@
 #!/usr/bin/env bash
 
+LATEST_COMMIT=$(git rev-parse --short HEAD)
+VERSIONS=$(ls -t releases)
+KEEP_OLDER_VERSIONS=0 # Change this to keep 1 or more older versions
+
+# To revert live to an older version run:
+# ln -sf releases/short-commit-hash releases/latest
+
+# Generate new version
 npx react-scripts build
-rsync -r --delete build/* release
+mkdir -p "releases/$LATEST_COMMIT"
+rsync -r --delete build/* "releases/$LATEST_COMMIT"
+ln -sf "releases/$LATEST_COMMIT" "releases/latest"
 rm -rf build
+
+# Remove old versions
+VERSIONS_COUNT=0
+for VERSION in $VERSIONS
+do
+  if [ "$VERSION" == "$LATEST_COMMIT" ]
+  then
+    continue
+  fi
+
+  if [ "$VERSION" == "latest" ]
+  then
+    continue
+  fi
+
+  if [ $VERSIONS_COUNT -eq $KEEP_OLDER_VERSIONS ]
+  then
+    rm -rf "releases/$VERSION"
+    continue
+  fi
+  VERSIONS_COUNT=$((VERSIONS_COUNT + 1))
+done
